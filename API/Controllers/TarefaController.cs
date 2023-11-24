cf8d0d8cd230cd49f2d3d87e0debb676c7ad814c
vanessamaguolo <vanessa-cristine_ctba@hotmail.com>
2023-11-24 10:41:32 -0300

    API feita

--- API/Controllers/TarefaController.cs
+++ API/Controllers/TarefaController.cs
@@ -14,6 +14,81 @@ public class TarefaController : ControllerBase
     public TarefaController(AppDataContext context) =>
         _context = context;
 
+    // GET:	api/tarefa/concluidas
+    [HttpGet]
+    [Route("concluidas")]
+    public IActionResult Concluidas()
+    {
+        try
+        {
+            List<Tarefa> Tarefas = null;
+            List<Tarefa> TarefasTemp = _context.Tarefas.Include(x => x.Categoria).ToList();
+            foreach (Tarefa tarefa in TarefasTemp)
+            {
+                if(tarefa.Status == "Concluída"){
+                    Tarefas.Add(tarefa);
+                }
+            }
+            return Ok(Tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    
+    
+    // GET:	api/tarefa/naoconcluidas
+    [HttpGet]
+    [Route("naoconcluidas")]
+    public IActionResult NaoConcluidas()
+    {
+        try
+        {
+            List<Tarefa> Tarefas = null;
+            List<Tarefa> TarefasTemp = _context.Tarefas.Include(x => x.Categoria).ToList();
+            foreach (Tarefa tarefa in TarefasTemp)
+            {
+                if(tarefa.Status == "Não iniciada" || tarefa.Status == "Em andamento"){
+                    Tarefas.Add(tarefa);
+                }
+            }
+            return Ok(Tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    // PATCH: api/tarefa/alterar
+    [HttpPatch]
+    [Route("alterar")]
+    public IActionResult Alterar([FromRoute] int id, [FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            Tarefa? tarefaCadastrada =
+                _context.Tarefas.FirstOrDefault(x => x.TarefaId == id);
+            if (tarefaCadastrada != null)
+            {
+                if(tarefaCadastrada.Status == "Não iniciada"){
+                    tarefaCadastrada.Status = "Em andamento";
+                }else if(tarefaCadastrada.Status == "Em andamento"){
+                    tarefaCadastrada.Status = "Concluída";
+                }
+                _context.SaveChanges();
+                return Ok(tarefa);
+            }
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
     // GET: api/tarefa/listar
     [HttpGet]
     [Route("listar")]
@@ -43,6 +118,7 @@ public class TarefaController : ControllerBase
                 return NotFound();
             }
             tarefa.Categoria = categoria;
+            tarefa.Status = "Não iniciada";
             _context.Tarefas.Add(tarefa);
             _context.SaveChanges();
             return Created("", tarefa);
